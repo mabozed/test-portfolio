be53c72e05c8623675fb6c93cea53b5f84355b82
mabozed <mabozed.com@gmail.com>
2024-03-20 02:09:51 +0300

    fix 11

--- src/App.jsx
+++ src/App.jsx
@@ -7,27 +7,24 @@ import Contact from './pages/Contact/Contact'
 import { ThemeContext } from './Context/ThemeContext'
 import { useEffect, useState } from 'react'
 
-const router = createBrowserRouter(
-  [
-    {
-      path: '/',
-      element: <Home />,
-    },
-    {
-      path: '/contact',
-      element: <Contact />,
-    },
-    {
-      path: '/resume',
-      element: <Resume />,
-    },
-    {
-      path: '/work',
-      element: <Work />,
-    },
-  ],
-  { basename: '/test-portfolio/' }
-)
+const router = createBrowserRouter([
+  {
+    path: '/test-portfolio/',
+    element: <Home />,
+  },
+  {
+    path: '/test-portfolio/contact',
+    element: <Contact />,
+  },
+  {
+    path: '/test-portfolio/resume',
+    element: <Resume />,
+  },
+  {
+    path: '/test-portfolio/work',
+    element: <Work />,
+  },
+])
 
 function App() {
   const [theme, setTheme] = useState(
