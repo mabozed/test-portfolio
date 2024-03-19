8b7d4ed00fd9fbb4da71c8f6f9784d07b3a36817
mabozed <mabozed.com@gmail.com>
2024-03-20 00:34:50 +0300

    fix 6

--- src/components/Navbar/Navbardetail.jsx
+++ src/components/Navbar/Navbardetail.jsx
@@ -1,26 +1,26 @@
 export const ItemsDetail = [
-    {
-        title : "Home",
-        url : "/",
-        className : "navitems",
-        icon : "fa-solid fa-house-chimney",
-    },
-    {
-        title : "Resume",
-        url : "/resume",
-        className : "navitems",
-        icon : "fa-solid fa-file",
-    },
-    {
-        title : "Work",
-        url : "/work",
-        className : "navitems",
-        icon : "fa-solid fa-briefcase",
-    },
-    {
-        title : "Contact",
-        url : "/contact",
-        className : "navitems",
-        icon : "fa-solid fa-address-book",
-    },
+  {
+    title: 'Home',
+    url: '/test-portfolio/',
+    className: 'navitems',
+    icon: 'fa-solid fa-house-chimney',
+  },
+  {
+    title: 'Resume',
+    url: '/test-portfolio/resume',
+    className: 'navitems',
+    icon: 'fa-solid fa-file',
+  },
+  {
+    title: 'Work',
+    url: '/test-portfolio/work',
+    className: 'navitems',
+    icon: 'fa-solid fa-briefcase',
+  },
+  {
+    title: 'Contact',
+    url: '/test-portfolio/contact',
+    className: 'navitems',
+    icon: 'fa-solid fa-address-book',
+  },
 ]
