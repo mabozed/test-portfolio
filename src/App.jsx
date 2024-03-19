b625b108cdcedb339cc83524b36a4b4d60f2dea2
mabozed <mabozed.com@gmail.com>
2024-03-20 00:31:58 +0300

    fix 5

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,39 @@
-import { Routes, Route } from "react-router-dom";
-import "./App.css";
-import Home from "./pages/Home/Home";
-import Resume from "./pages/Resume/Resume";
-import Work from "./pages/Work/Work";
-import Contact from "./pages/Contact/Contact";
-import { ThemeContext } from "./Context/ThemeContext";
-import { useEffect, useState } from 'react';
+import { Routes, Route } from 'react-router-dom'
+import './App.css'
+import Home from './pages/Home/Home'
+import Resume from './pages/Resume/Resume'
+import Work from './pages/Work/Work'
+import Contact from './pages/Contact/Contact'
+import { ThemeContext } from './Context/ThemeContext'
+import { useEffect, useState } from 'react'
 
 function App() {
-  const [theme, setTheme] = useState(localStorage.getItem("theme") ? localStorage.getItem("theme"):"light");
-  localStorage.setItem("theme", theme);
+  const [theme, setTheme] = useState(
+    localStorage.getItem('theme') ? localStorage.getItem('theme') : 'light'
+  )
+  localStorage.setItem('theme', theme)
 
   function ToggleChangeMode() {
-    setTheme((prev) => (prev === "dark" ? "light" : "dark"));
+    setTheme((prev) => (prev === 'dark' ? 'light' : 'dark'))
   }
-  
-  useEffect(() =>
-  {
-    setTheme(localStorage.getItem("theme"))
-      ,[]})
+
+  useEffect(() => {
+    setTheme(localStorage.getItem('theme')), []
+  })
   return (
     <>
-    <ThemeContext.Provider value={{ theme, ToggleChangeMode }}>
-      <div className={theme}>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/Resume" element={<Resume/>} />
-          <Route path="/Work" element={<Work/>} />
-          <Route path="/Contact" element={<Contact />} />
-        </Routes>
-      </div>
+      <ThemeContext.Provider value={{ theme, ToggleChangeMode }}>
+        <div className={theme}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/test-portfolio/Resume" element={<Resume />} />
+            <Route path="/test-portfolio/Work" element={<Work />} />
+            <Route path="/test-portfolio/Contact" element={<Contact />} />
+          </Routes>
+        </div>
       </ThemeContext.Provider>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
