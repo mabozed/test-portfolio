76c725c5d1ed78e5d5bfa8083531074b142cf083
mabozed <mabozed.com@gmail.com>
2024-03-20 02:31:40 +0300

    fix 14

--- src/components/Workproject/Workdata.js
+++ src/components/Workproject/Workdata.js
@@ -1,21 +1,20 @@
-
 export const Data = [
-    {
-        name : "index",
-        description : "World's Biggest University",
-        picture : "../images/project1.jpg",
-        url : " https://eng-monasaleh.github.io/project1 ",
-    },
-    {
-        name : "value",
-        description : "Hotelo",
-        picture : "../images/project2.jpg",
-        url : " https://eng-monasaleh.github.io/mona-project2 ",
-    },
-    {   
-        name : "index1",
-        description : "HomeVerse",
-        picture :"../images/project3.jpg",
-        url : "https://eng-monasaleh.github.io/re-project",
-    }
+  {
+    name: 'index',
+    description: "World's Biggest University",
+    picture: '/test-portfolio/images/project1.jpg',
+    url: ' https://eng-monasaleh.github.io/project1 ',
+  },
+  {
+    name: 'value',
+    description: 'Hotelo',
+    picture: '/test-portfolio/images/project2.jpg',
+    url: ' https://eng-monasaleh.github.io/mona-project2 ',
+  },
+  {
+    name: 'index1',
+    description: 'HomeVerse',
+    picture: '/test-portfolio/images/project3.jpg',
+    url: 'https://eng-monasaleh.github.io/re-project',
+  },
 ]
